0eb9f9e57fc7635933b895c2cdf1f8ef41ec05d7
Yana <litvinchikyana@ukr.net>
2018-05-22 16:58:05 +0300

    Add project files.

---
+++ WindowsFormsApplication3/MainClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication3
+{
+    class MainClass
+    {
+        List<Questions> q = null;
+        public MainClass()
+        {
+            q = new List<Questions>()
+            {
+                new Questions("How many legs do u have?",1, "2", "10", "4", "-1"),
+                new Questions("2 + 2*2 = ?", 2, "8", "6", "12", "4"),
+                new Questions("С какой скоростью полетит маркер если не ответить Чумаку на вопрос о С# ?", 2, "300 000 km/h", "7 m/s", "15 m/s", "1 m/s"),
+                new Questions("sin(30)?", 3 , "1/2", "5.44444", "3/4", "no answer"),
+                new Questions("Сколько занимает памяти int?"),
+                new Questions("Speed of light?"),
+                new Questions("How old is Dima?"),
+            };
+        }
+    }
+}
---
+++ WindowsFormsApplication3/Questions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication3
+{
+    [Serializable]
+    class Questions
+    {
+        public string Question { get; set; }
+        public List<string> Answers { get; set; }
+        public int Key { get; set; }
+
+        public Questions(string q, int key, params string[]an)
+        {
+            this.Question = q;
+            this.Key = key;
+            this.Answers = new List<string>(an);
+        }
+
+    }
+}
